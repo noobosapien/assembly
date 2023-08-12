121d9b834f0e4f3747c41793037d1ba41aacd978
noobosapien <migirulz44@gmail.com>
2023-08-11 23:30:47 -0800

    init

---
+++ test2.asm
@@ -0,0 +1,19 @@
+format ELF executable 3
+entry _start
+
+segment readable executable
+_start:
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, message
+	mov edx, len
+	int 0x80
+
+	xor ebx, ebx
+	mov eax, ebx
+	inc eax
+	int 0x80
+
+segment readable writable
+	message db "Hello from FASM", 0x0a
+	len = $ - message
